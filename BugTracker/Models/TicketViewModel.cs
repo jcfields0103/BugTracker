747c92f80c6bac28be07d4a7389c64ea4c913352
jcfields0103 <jcfields0103@users.noreply.github.com>
2019-08-19 09:51:47 -0400

    8192

---
+++ BugTracker/Models/TicketViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.Models
+{
+    public class TicketViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int TicketPriorityId { get; set; }
+        public int TicketStatusId { get; set; }
+        public int TicketTypeId { get; set; }
+        public DateTime Created { get; set; }
+        public IEnumerable<SelectListItem> CurrentPriority { get; set; }
+        public IEnumerable<SelectListItem> CurrentStatus { get; set; }
+        public IEnumerable<SelectListItem> CurrentType { get; set; }
+    }
+}
---
+++ BugTracker/Models/UserProfileViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BugTracker.Models
+{
+    public class UserProfileViewModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Email { get; set; }
+    }
+}
